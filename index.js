78cac7d3ee4895e5aa51d38434cb3ce1cb639c70
ymdsy <yamada@example.com>
2019-11-30 13:17:27 +0900

    trycatchで非同期処理のエラーハンドリングできないことの確認

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-const http = require("http");
 const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
-  res.end();
-});
+console.log("started!!");
 
-server.listen(8080, "127.0.0.1", () => {
-  console.log("started!!");
-
-  fs.readFile("exampleFile.txt", (err, data) => {
-    // fs.readFile("dummyFile.txt", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+try {
+  //   fs.readFile("exampleFile.txt", (err, data) => {
+  fs.readFile("dummyFile.txt", (err, data) => {
+    //   if (err) {
+    console.error(err);
+    //     return;
+    //   }
 
     console.log(data.toString());
   });
+} catch (err) {
+  console.error(err);
+}
 
-  console.log("end");
-});
+console.log("end");
