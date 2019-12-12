e8a3e541a7ece3b8a7cd1bef820a680b9f31cac2
ymdsy <yamada@example.com>
2019-12-12 23:30:09 +0900

    async/awaitを使ったファイル書き込み

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
 console.log("started!!");
 
-const write = i => {
-  if (i > 100) return;
-
-  fs.appendFile("exampleFile.txt", i++ + ",", err => {
-    write(i);
-  });
+const write = async () => {
+  for (let i = 0; i < 100; i++) {
+    await fs.appendFile("exampleFile.txt", i + ",");
+  }
 };
 
 write(0);
